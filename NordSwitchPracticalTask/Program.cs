94134ab5cc3d94ee5918801650003d3771a03184
Patrikas Adomaitis <ado.patrikas@gmail.com>
2022-11-15 18:42:47 +0200

    Code now uses Params

--- NordSwitchPracticalTask/Program.cs
+++ NordSwitchPracticalTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace NordSwitchPracticalTask
 {
@@ -6,7 +7,38 @@ namespace NordSwitchPracticalTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = null;
+            string t = null;
+            while (true)
+            {
+                if (args.Length == 0 || args[0] == t)
+                {
+                    Console.WriteLine("Please enter file name");
+                    t = Console.ReadLine();
+                }
+                else
+                    t = args[0];
+
+                path = Environment.CurrentDirectory + $"\\{t}";
+
+                if (File.Exists(path))
+                    break;
+                Console.WriteLine("File does not exist");
+            }
+            Console.WriteLine("File exists!");
+
+            var text = File.ReadAllText(t);
+
+            Program p = new Program();
+            p.CheckText(text);
+
+
+            Console.ReadKey();
+        }
+
+        void CheckText(string text)
+        {
+            Console.WriteLine(text);
         }
     }
 }
